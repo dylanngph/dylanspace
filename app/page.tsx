acf9600e040535e6c579710a134e4afb2ab5d40a
duyngph <duyngph@gmail.com>
2023-10-02 22:54:08 +0700

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@nextui-org/button";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <Button color="primary">Press me</Button>
+      </div>
+    </main>
+  );
+}
