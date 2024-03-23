4c330d9053609e0dac0ec6bca937f8dead9d7261
dylanngph <duyngph@gmail.com>
2024-03-23 19:12:38 +0700

    Refactor theme provider component

--- providers/theme-provider.tsx
+++ providers/theme-provider.tsx
@@ -2,21 +2,18 @@
 
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { LazyMotion, domAnimation } from "framer-motion";
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <LazyMotion features={domAnimation}>
-      <NextUIProvider>
-        <NextThemesProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </NextThemesProvider>
-      </NextUIProvider>
-    </LazyMotion>
+    <NextUIProvider>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </NextThemesProvider>
+    </NextUIProvider>
   );
 }
